0ab5fe752ef543097e57beffc070ff618583ea4c
Khari2003 <99896250+Khari2003@users.noreply.github.com>
2025-04-30 01:15:40 +0700

    hostname

--- app.js
+++ app.js
@@ -19,4 +19,4 @@ mongoose.connect(process.env.MONGODB_CONNECT)
     .catch(err => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on 0.0.0.0:${PORT}`));
