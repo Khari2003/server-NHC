011becda779079e889cb8dcf5bdcdede38e4ed8a
Khari2003 <99896250+Khari2003@users.noreply.github.com>
2025-05-02 16:35:55 +0700

    update cors

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const router = require('./router/index');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
